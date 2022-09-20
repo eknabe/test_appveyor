6501a8ef6a5a4e2ed5f326c6a4a6c341cdeb0871
eknabevcc <emil.knabe@volvocars.com>
2022-09-20 12:51:16 +0200

    Add scripts

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>   
 
 int main(int argc, char* argv[])
 {
-    int return_value = argc > 1 ? atoi(argv[1]) : -1;
+    printf("Hello Anders and rest of the world!\n");
     
-    printf("Hello! Returning %d.\n", return_value);
-    
-    return return_value;
+    return 0;
 }
